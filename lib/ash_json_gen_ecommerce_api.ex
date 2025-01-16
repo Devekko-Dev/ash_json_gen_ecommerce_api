0a21ed2652a1792f171f6860ffc1c328c0a18dda
Nicholas Roberts <niccolox@devekko.com>
2025-01-15 22:50:27 -0800

    ash json wrapper generator ecommerce api

---
+++ lib/ash_json_gen_ecommerce_api.ex
@@ -0,0 +1,18 @@
+defmodule AshJsonGenEcommerceApi do
+  @moduledoc """
+  Documentation for `AshJsonGenEcommerceApi`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AshJsonGenEcommerceApi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ash_json_gen_ecommerce_api_test.exs
@@ -0,0 +1,8 @@
+defmodule AshJsonGenEcommerceApiTest do
+  use ExUnit.Case
+  doctest AshJsonGenEcommerceApi
+
+  test "greets the world" do
+    assert AshJsonGenEcommerceApi.hello() == :world
+  end
+end
